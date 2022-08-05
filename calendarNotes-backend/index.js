3fd8a25ac7ee512b1f19784b302dbe7b22ba098e
Luis <luismgval@gmail.com>
2022-08-05 17:14:27 +0200

    env

--- calendarNotes-backend/index.js
+++ calendarNotes-backend/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 4000
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
 })
 
 
-app.get('/', (req, res) => {
+//drirectory path
+app.use(express.static('public'))
+
+/* app.get('/', (req, res) => {
   res.json({
     oh:true
   })
-})
+}) */
